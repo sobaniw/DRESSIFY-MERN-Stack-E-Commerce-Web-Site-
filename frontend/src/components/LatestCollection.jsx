c2ac89d1f47e087b5199029e1cc6019da78827ac
sobaniw <sobani.darshi@gmail.com>
2026-04-27 12:04:04 +0530

    first pushing the code

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
+
+const LatestCollection = () => {
+  const { products, currency } = useContext(ShopContext);
+  const latestProducts = products.slice(0, 10);
+
+  return (
+    <div className="my-10">
+      <div className="text-center text-3xl py-8">
+        <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item) => (
+          <div key={item._id} className="cursor-pointer">
+            <div className='overflow-hidden'>
+              <img
+                src={Array.isArray(item.image) ? item.image[0] : item.image}
+                alt={item.name}
+                className="w-full object-cover hover:scale-110 transition ease-in-out"
+              />
+            </div>
+            <p className="pt-3 pb-1 text-sm">{item.name}</p>
+            <p className="text-sm font-medium">{currency}{item.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default LatestCollection
